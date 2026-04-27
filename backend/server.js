f2cc243e3474f01cbe70f589e209c696d0ee388d
alien <aliendevuz@gmail.com>
2026-04-27 10:03:27 +0500

    backend: CORS fixed again

--- backend/server.js
+++ backend/server.js
@@ -16,17 +16,19 @@ connectDB();
 const allowedOrigins = [
   "http://localhost:5500",
   "http://localhost:5501",
+  "http://127.0.0.1:5500",
+  "http://127.0.0.1:5501",
   "https://campus-302.netlify.app",
 ];
 
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
+      // if (!origin || allowedOrigins.includes(origin)) {
         return callback(null, true);
-      }
+      // }
 
-      return callback(new Error("Not allowed by CORS"));
+      // return callback(new Error("Not allowed by CORS"));
     },
   })
 );
